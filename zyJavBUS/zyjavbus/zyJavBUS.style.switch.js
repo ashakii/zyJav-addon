d0a7f1b36dacc0e9b1ca5ae6fc40ceb61abce405
zyashakii <39474823+ashakii@users.noreply.github.com>
2025-04-09 14:46:24 +0800

    Update zyJavBUS.style.switch.js

--- zyJavBUS/zyjavbus/zyJavBUS.style.switch.js
+++ zyJavBUS/zyjavbus/zyJavBUS.style.switch.js
@@ -1,15 +1,20 @@
 // ==UserScript==
-// @name            zyJavBUS.style
+// @name            zyJavBUS.style.switch
 // @version         0.1
 // @author          zyashakii
 // @description     样式调整
 // @match           https://www.javbus.com/*
 // @icon            https://www.javbus.com/favicon.ico
-// @resource        style https://raw.githubusercontent.com/ashakii/javlibs/refs/heads/main/Javbus/static/JavBUS.style.user.css
+// @resource        zystyle https://raw.githubusercontent.com/ashakii/zyJav-addon/refs/heads/main/zyJavBUS/static/zyJavbus.zystyle.css
+// @resource        style3 https://raw.githubusercontent.com/ashakii/zyJav-addon/refs/heads/main/zyJavBUS/static/JavBUS.style3.css
+// @resource        style4 https://raw.githubusercontent.com/ashakii/zyJav-addon/refs/heads/main/zyJavBUS/static/JavBUS.style4.css
+// @updateURL       https://raw.githubusercontent.com/ashakii/zyJav-addon/refs/heads/main/zyJavBUS/zyjavbus/zyJavBUS.style.switch.js
 // @run-at          document-start
 // @grant           GM_getResourceText
 // @grant           GM_addStyle
 // ==/UserScript==
 
-// 修改JAVBUS页面的样式并替换图片
-GM_addStyle(GM_getResourceText("style"));
+// 修改JAVBUS页面的样式
+GM_addStyle(GM_getResourceText("zystyle"));
+GM_addStyle(GM_getResourceText("style3")); //3列
+//GM_addStyle(GM_getResourceText("style4")); //4列

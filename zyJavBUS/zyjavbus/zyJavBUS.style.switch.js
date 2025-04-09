353dae2e4a010837a7ef1fe0900bce08821b0a75
zyashakii <39474823+ashakii@users.noreply.github.com>
2025-04-09 23:24:07 +0800

    Update zyJavBUS.style.switch.js

--- zyJavBUS/zyjavbus/zyJavBUS.style.switch.js
+++ zyJavBUS/zyjavbus/zyJavBUS.style.switch.js
@@ -8,6 +8,8 @@
 // @resource        zystyle https://raw.githubusercontent.com/ashakii/zyJav-addon/refs/heads/main/zyJavBUS/static/zyJavbus.zystyle.css
 // @resource        style3 https://raw.githubusercontent.com/ashakii/zyJav-addon/refs/heads/main/zyJavBUS/static/JavBUS.style3.css
 // @resource        style4 https://raw.githubusercontent.com/ashakii/zyJav-addon/refs/heads/main/zyJavBUS/static/JavBUS.style4.css
+// @resource        style5 https://raw.githubusercontent.com/ashakii/zyJav-addon/refs/heads/main/zyJavBUS/static/JavBUS.style5.css
+// @resource        style6 https://raw.githubusercontent.com/ashakii/zyJav-addon/refs/heads/main/zyJavBUS/static/JavBUS.style6.css
 // @updateURL       https://raw.githubusercontent.com/ashakii/zyJav-addon/refs/heads/main/zyJavBUS/zyjavbus/zyJavBUS.style.switch.js
 // @run-at          document-start
 // @grant           GM_getResourceText
@@ -18,3 +20,5 @@
 GM_addStyle(GM_getResourceText("zystyle"));
 GM_addStyle(GM_getResourceText("style3")); //3列
 //GM_addStyle(GM_getResourceText("style4")); //4列
+//GM_addStyle(GM_getResourceText("style5")); //5列
+//GM_addStyle(GM_getResourceText("style6")); //6列

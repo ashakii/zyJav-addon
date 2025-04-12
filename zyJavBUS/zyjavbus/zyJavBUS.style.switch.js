67f4948ea3204cd644fc2c6a65819d08c9d40a0e
zyashakii <39474823+ashakii@users.noreply.github.com>
2025-04-12 09:26:49 +0800

    Update zyJavBUS.style.switch.js

--- zyJavBUS/zyjavbus/zyJavBUS.style.switch.js
+++ zyJavBUS/zyjavbus/zyJavBUS.style.switch.js
@@ -6,19 +6,49 @@
 // @match           https://www.javbus.com/*
 // @icon            https://www.javbus.com/favicon.ico
 // @resource        zystyle https://raw.githubusercontent.com/ashakii/zyJav-addon/refs/heads/main/zyJavBUS/static/zyJavbus.zystyle.css
-// @resource        style3 https://raw.githubusercontent.com/ashakii/zyJav-addon/refs/heads/main/zyJavBUS/static/JavBUS.style3.css
-// @resource        style4 https://raw.githubusercontent.com/ashakii/zyJav-addon/refs/heads/main/zyJavBUS/static/JavBUS.style4.css
-// @resource        style5 https://raw.githubusercontent.com/ashakii/zyJav-addon/refs/heads/main/zyJavBUS/static/JavBUS.style5.css
-// @resource        style6 https://raw.githubusercontent.com/ashakii/zyJav-addon/refs/heads/main/zyJavBUS/static/JavBUS.style6.css
-// @updateURL       https://raw.githubusercontent.com/ashakii/zyJav-addon/refs/heads/main/zyJavBUS/zyjavbus/zyJavBUS.style.switch.js
+// @resource        style https://raw.githubusercontent.com/ashakii/zyJav-addon/refs/heads/main/zyJavBUS/static/JavBUS.style.css
+// @updateURL       https://raw.githubusercontent.com/ashakii/zyJav-addon/main/zyJavBUS/zyjavbus/zyJavBUS.style.switch.js
+// @downloadURL     https://raw.githubusercontent.com/ashakii/zyJav-addon/main/zyJavBUS/zyjavbus/zyJavBUS.style.switch.js
 // @run-at          document-start
 // @grant           GM_getResourceText
 // @grant           GM_addStyle
 // ==/UserScript==
 
 // 修改JAVBUS页面的样式
+const styles = {
+  style3: `
+    #waterfall .item:not(.item:has(.avatar-box)) {
+      max-width: 30%;
+      min-width: 260px;
+      display: flex;
+    }
+  `,
+  style4: `
+    #waterfall .item:not(.item:has(.avatar-box)) {
+      max-width: 23%;
+      min-width: 260px;
+      display: flex;
+    }
+  `,
+  style5: `
+    #waterfall .item:not(.item:has(.avatar-box)) {
+      max-width: 19%;
+      min-width: 260px;
+      display: flex;
+    }
+  `,
+  style6: `
+    #waterfall .item:not(.item:has(.avatar-box)) {
+      max-width: 15%;
+      min-width: 260px;
+      display: flex;
+    }
+  `,
+};
+
 GM_addStyle(GM_getResourceText("zystyle"));
-GM_addStyle(GM_getResourceText("style3")); //3列
-//GM_addStyle(GM_getResourceText("style4")); //4列
-//GM_addStyle(GM_getResourceText("style5")); //5列
-//GM_addStyle(GM_getResourceText("style6")); //6列
+GM_addStyle(GM_getResourceText("style"));
+// GM_addStyle(styles.style3);
+// GM_addStyle(styles.style4);
+// GM_addStyle(styles.style5);
+GM_addStyle(styles.style6);
